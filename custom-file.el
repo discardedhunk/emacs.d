c8a19218ac8fcd97a879ddb638a80a5668992a22
Jason LaCarrubba <jlacarrubba@Jason-LaCarrubba.local>
2013-03-11 16:56:34 -0400

    initial

---
+++ custom-file.el
@@ -0,0 +1,42 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 215 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(auto-save-timeout 10)
+ '(default-frame-alist (quote ((vertical-scroll-bars . right) (menu-bar-lines . 1) (tool-bar-lines . 0) (fringe) (right-fringe) (left-fringe . 1) (internal-border-width . 0) (cursor-type . box) (foreground-color . "#839496") (background-color . "#002b36") (background-mode . dark) (cursor-color . "#839496"))))
+ '(display-time-mode t)
+ '(global-linum-mode t)
+ '(ido-case-fold nil)
+ '(ido-confirm-unique-completion nil)
+ '(ido-enable-dot-prefix t)
+ '(ido-enable-flex-matching t)
+ '(ido-enter-matching-directory (quote first))
+ '(ido-ignore-buffers (quote ("\\` " "^\\*.*\\*$")))
+ '(inhibit-startup-screen t)
+ '(js2-auto-indent-flag nil)
+ '(js2-bounce-indent-p t)
+ '(js2-enter-indents-newline nil)
+ '(js2-highlight-level 3)
+ '(js2-indent-on-enter-key nil)
+ '(js2-mirror-mode nil)
+ '(js2-mode-indent-ignore-first-tab t)
+ '(ns-tool-bar-display-mode nil t)
+ '(ns-tool-bar-size-mode nil t)
+ '(org-agenda-files (quote ("~/docs/notes/todo.org")))
+ '(org-file-apps (quote (("txt" . emacs) ("tex" . emacs) ("ltx" . emacs) ("org" . emacs) ("el" . emacs) ("bib" . emacs) ("pdf" . "evince '%s'"))))
+ '(org-hide-leading-stars t)
+ '(org-log-done (quote time))
+ '(org-odd-levels-only t)
+ '(org-startup-folded (quote content))
+ '(safe-local-variable-values (quote ((encoding . utf-8) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby"))))
+ '(visual-line-mode nil t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
